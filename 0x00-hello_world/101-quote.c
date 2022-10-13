290d384924bc4a5d4b9b946e12abe1b73a0cc2ae
Eseoghene-ODODO <en.ododohotmail.com>
2022-10-13 08:06:23 -0700

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: using printf function to output a stderr
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fprintf(stderr,
+			"with proper grammar, but the outcome is a piece of art,\n");
+	return (1);
+}
