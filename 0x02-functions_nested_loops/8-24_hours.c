100b1d1d17a14ee1b722995268a3a41b978a20ca
Eseoghene-ODODO <en.ododohotmail.com>
2022-10-20 03:29:30 -0700

    function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,9 +13,9 @@ void jack_bauer(void)
 	int hours = 0;
 	int minutes = 0;
 
-	while (hours < 23)
+	while (hours <= 23)
 	{
-		while (minutes < 59)
+		while (minutes <= 59)
 		{
 			_putchar(hours / 10 + '0');
 			_putchar(hours % 10 + '0');
