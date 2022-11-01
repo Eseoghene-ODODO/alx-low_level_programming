c734e0254dab4276bac6aaffd010da4281f6b260
Eseoghene-ODODO <en.ododohotmail.com>
2022-10-31 21:22:38 -0700

    function that copies memory area.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* memset - Fills the first n bytes of the memory
+* @s: pointer.
+* @c: character.
+* @n: number.
+* Return: memory
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+        unsigned int index;
+        unsigned char memory[] = s;
+        unsigned char value = b;
+        index = 0;
+
+        while (index < n)
+        {
+                index++;
+                memory[index] = value;
+        }
+        return (memory);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Solutions to task 0x07. C - Even more pointers, arrays and strings
