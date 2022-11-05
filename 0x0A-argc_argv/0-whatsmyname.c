05f6625d868db8d80cc8cc00047a5257edaaf7d1
Eseoghene-ODODO <en.ododohotmail.com>
2022-11-04 22:43:59 -0700

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,13 @@
 /**
  * main - function thats outputs the string
  * @argc: integer
- * @argv[]: character
+ * @argv: character
  * Return: 0
  */
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
