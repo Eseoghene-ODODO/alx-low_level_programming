6953f147fc3876e1608cd0b74e007921b55bad3d
Eseoghene-ODODO <en.ododohotmail.com>
2022-11-08 02:19:50 -0800

    function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,13 @@ char *str_concat(char *s1, char *s2)
 	char *concat_str;
 	int index, concat_index = 0, len = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	for (index = 0; s1[index]; index++)
 	{
