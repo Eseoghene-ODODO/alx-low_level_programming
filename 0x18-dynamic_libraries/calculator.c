03e2515e3844c15d4013570eab25abfb65a04501
Eseoghene-ODODO <en.ododohotmail.com>
2023-01-16 06:11:45 -0800

    Create a dynamic library that contains C functions that can be called from Python.

--- 0x18-dynamic_libraries/calculator.c
+++ 0x18-dynamic_libraries/calculator.c
@@ -10,7 +10,7 @@ int mul(int a, int b)
 {
 	return (a * b);
 }
-int divn(int a, int b)
+int div(int a, int b)
 {
 	if (b != 0)
 	{	return (a / b);
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -21,10 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int divn(int a, int b);
-int mod(int a, int b);
 
 #endif /*main_h*/
