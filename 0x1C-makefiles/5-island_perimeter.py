f11d9d3957c8e33529b3c07e229b5624bb58228b
Eseoghene-ODODO <en.ododohotmail.com>
2023-03-29 14:14:33 -0700

    Solution to task 0-100

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,31 +1,30 @@
-#!/usr/bin/env python3
-"""Module that calculates the perimeter of an island in a grid."""
-
-
-def num_water_neighbors(grid, i, j):
-    """Returns the number of water neighbors a cell has in a grid."""
-
-    num = 0
-
-    if i <= 0 or not grid[i - 1][j]:
-        num += 1
-    if j <= 0 or not grid[i][j - 1]:
-        num += 1
-    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
-        num += 1
-    if i >= len(grid) - 1 or not grid[i + 1][j]:
-        num += 1
-
-    return num
+#!/usr/bin/python3
+"""Module for the island_perimeter method"""
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island in grid."""
-
+    """methos for calculate island perimeter"""
+    max_col = len(grid[0])
+    max_row = len(grid)
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j]:
-                perimeter += num_water_neighbors(grid, i, j)
 
+    for i in range(0, max_row):
+        for j in range(0, max_col):
+            if grid[i][j] == 1:
+                if i == 0:
+                    perimeter += 1
+                elif grid[i-1][j] == 0:
+                    perimeter += 1
+                if i == max_row - 1:
+                    perimeter += 1
+                elif grid[i+1][j] == 0:
+                    perimeter += 1
+                if j == 0:
+                    perimeter += 1
+                elif grid[i][j-1] == 0:
+                    perimeter += 1
+                if j == max_col - 1:
+                    perimeter += 1
+                elif grid[i][j+1] == 0:
+                    perimeter += 1
     return perimeter
