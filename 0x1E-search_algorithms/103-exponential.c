c901c2cb65639be163b41a12a2d301af4525d992
Eseoghene-ODODO <en.ododo@gmail.com>
2023-05-25 12:03:08 -0700

    Solution to task 9-14

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -2,7 +2,7 @@
 #include "search_algos.h"
 
 /**
- * exponential_search - Searches for a value in a sorted array of integers
+ * binary_search - Searches for a value in a sorted array of integers
  *                      using the Exponential search algorithm.
  *
  * @array: Pointer to the first element of the array to search in.
