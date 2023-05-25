5350008ea621aafff663bb2e92359b4a9e6a844e
Eseoghene-ODODO <en.ododo@gmail.com>
2023-05-25 08:47:37 -0700

    Solution to task 9-14

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,5 +1,5 @@
 #include <stdio.h> /* include the header file for printf */
-#include "search_algos.h" /* include the header file for interpolation_search */
+#include "search_algos.h"
 
 /**
  * interpolation_search - searches for a value in a sorted array of integers
@@ -7,38 +7,41 @@
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
- * Return: first index where value is located, or -1 if not found or array is NULL
+ * Return: first index where value is located, or
+ * -1 if not found or array is NULL
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-    size_t low, high, pos;
+	size_t low, high, pos;
 
-    if (array == NULL || size == 0) /* check for invalid input */
-        return (-1);
+	if (array == NULL || size == 0) /* check for invalid input */
+		return (-1);
 
-    low = 0;
-    high = size - 1;
+	low = 0;
+	high = size - 1;
 
-    while (low <= high) /* loop until value is found or not */
-    {
-        pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low])); /* calculate the probe position */
+	while (low <= high) /* loop until value is found or not */
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low])) *
+			     (value - array[low])); /* calculate the probe position */
 
-        if (pos >= size) /* check if position is out of range */
-        {
-            printf("Value checked array[%lu] is out of range\n", pos);
-            break;
-        }
+		if (pos >= size) /* check if position is out of range */
+		{
+			printf("Value checked array[%lu] is out of range\n", pos);
+			break;
+		}
 
-        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]); /* print the checked value */
+		printf("Value checked array[%lu] = [%d]\n",
+		       pos, array[pos]); /* print the checked value */
 
-        if (array[pos] == value) /* check if value is found */
-            return (pos);
+		if (array[pos] == value) /* check if value is found */
+			return (pos);
 
-        if (array[pos] < value) /* check if value is in the right half */
-            low = pos + 1;
-        else /* check if value is in the left half */
-            high = pos - 1;
-    }
+		if (array[pos] < value) /* check if value is in the right half */
+			low = pos + 1;
+		else /* check if value is in the left half */
+			high = pos - 1;
+	}
 
-    return (-1); /* value not found */
+	return (-1); /* value not found */
 }
