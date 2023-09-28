dda4053d1ea536371f28b195751ddc01f7623455
Eseoghene-ODODO <en.ododo@gmail.com>
2023-09-28 22:03:50 +0000

    Task 0 updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,21 +8,23 @@
  */
 unsigned int binary_to_uint(const char *b) 
 {
-    unsigned int result = 0;
+	unsigned int result = 0;
 
-    if (*b == '\0')
-    {
-        return 0;
-    }
-    while (*b) {
-        if (*b == '0' || *b == '1') {
-            result = result * 2 + (*b - '0');
-            b++;
-        }
-	else 
+	if (*b == '\0')
 	{
-            return 0;  /* Invalid character in the string */
-        }
-    }
-    return result;
+		return (0);
+	}
+	while (*b)
+	{
+		if (*b == '0' || *b == '1')
+		{
+			result = result * 2 + (*b - '0');
+			b++;
+		}
+		else
+		{
+			return (0);  /* Invalid character in the string */
+		}
+	}
+	return (result);
 }
