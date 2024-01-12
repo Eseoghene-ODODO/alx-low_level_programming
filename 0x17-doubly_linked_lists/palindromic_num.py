2ba0392d6e8b71b461de8444ea98fb2407d7c231
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-12 12:55:03 +0000

    102 task resolved

--- 0x17-doubly_linked_lists/palindromic_num.py
+++ 0x17-doubly_linked_lists/palindromic_num.py
@@ -8,15 +8,16 @@ from the product of two 3-digit numbers.
 def isPalindrome(number):
     return str(number) == str(number)[::-1]
 
-
 maxPalindrome = 0
 
-for i in range(100, 1000):
-    for j in range(100, 1000):
+for i in range(999, 99, -1):
+    for j in range(999, 99, -1):
         product = i * j
+        if product <= maxPalindrome:
+            break
         if isPalindrome(product) and product > maxPalindrome:
             maxPalindrome = product
 
-file = open("102-result", "w")
-file.write(str(maxPalindrome))
-file.close()
+with open("102-result", "w") as file:
+    file.write(str(maxPalindrome))
+
