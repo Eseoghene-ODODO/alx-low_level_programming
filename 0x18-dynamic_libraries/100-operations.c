977620406fd4e7bcb284bd77fca372fe6ed478ed
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-15 15:50:57 +0000

    task 100 solved

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,15 +1,42 @@
+/**
+ * add - function to add two integers a and b
+ * @a: integer value
+ * @b: integer value
+ * Return: a + b
+ */
 int add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * sub - function that subtracts value b from value a
+ * @a: integer value
+ * @b: integer value
+ * Return: a - b
+ */
 int sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+ * mul - function that multiplies two integers a and b
+ * @a: integer value
+ * @b: integer value
+ * Return: a * b
+ */
 int mul(int a, int b)
 {
 	return (a * b);
 }
+
+/**
+ * div - function that divides two integers a and b
+ * @a: integer value
+ * @b: integer value
+ * Return: a / b
+ */
 int div(int a, int b)
 {
 	if (b != 0)
@@ -17,6 +44,14 @@ int div(int a, int b)
 	}
 	return (0);
 }
+
+/**
+ * mod - function that outputs the remainder of a division
+ * of two integers a and b
+ * @a: integer value
+ * @b: integer value
+ * Return: a % b
+ */
 int mod(int a, int b)
 {
 	if (b != 0)
