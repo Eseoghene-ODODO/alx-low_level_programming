af9a3dce4e7964df410fbd9b77651fa076ec7c93
Eseoghene-ODODO <en.ododo@gmail.com>
2024-01-16 10:34:53 +0100

    101 resolved

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -fPIC -c 101-makemewin.c
+gcc -g -fPIC -Wall -pedantic -Werror -Wextra 101-makemewin.c -shared -o 101-makemewin.so
 LD_PRELOAD=/$PWD/101-makemewin.so ./gm 9 8 10 24 75 9
