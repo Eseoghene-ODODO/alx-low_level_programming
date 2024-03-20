6069e5d3db7f77a187f93350e0075f0c74c564c3
Eseoghene-ODODO <en.ododo@gmail.com>
2024-03-20 21:52:53 +0000

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,26 +10,25 @@ Parameters:
         One cell is a square with side length 1
         Grid cells are connected horizontally/vertically (not diagonally).
         Grid is rectangular, width and height don’t exceed 100
-        Grid is completely surrounded by water, and there is one island (or nothing).
-        The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
+        Grid is completely surrounded by water, and there is one island
+        (or nothing).
+        The island doesn’t have “lakes”
+        (water inside that isn’t connected to the water around the island).
 Returns:
     Perimeter of the island (integer)
 """
 
+
 def island_perimeter(grid):
     perimeter = 0
-    
     # Iterate through each cell in the grid
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            # If current cell is land, calculate its contribution to the perimeter
             if grid[i][j] == 1:
                 perimeter += 4
-                
                 # Check adjacent cells to subtract shared edges
                 if i > 0 and grid[i-1][j] == 1:
                     perimeter -= 2
                 if j > 0 and grid[i][j-1] == 1:
                     perimeter -= 2
-    
     return perimeter
