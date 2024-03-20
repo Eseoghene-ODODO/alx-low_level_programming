cbe145f705c64adc47495f0599769cc705d76a7f
Eseoghene-ODODO <en.ododo@gmail.com>
2024-03-20 21:54:43 +0000

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -20,13 +20,12 @@ Returns:
 
 
 def island_perimeter(grid):
+    """Function body"""
     perimeter = 0
-    # Iterate through each cell in the grid
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 perimeter += 4
-                # Check adjacent cells to subtract shared edges
                 if i > 0 and grid[i-1][j] == 1:
                     perimeter -= 2
                 if j > 0 and grid[i][j-1] == 1:
